81787883bb236720fbb28f84a9a0f06c40024eb3
daimiaopeng <32475719+daimiaopeng@users.noreply.github.com>
2019-11-02 23:49:14 +0800

    Update Server.h
    0.2

--- Server.h
+++ Server.h
@@ -8,9 +8,27 @@
 #include <string>
 #include <cstring>
 #include <iostream>
+#include <functional>
+#include <sys/epoll.h>
+#include <stdio.h>
+#include <memory>
+
 #define BUFF_MAX 1024
 #ifndef CHAT_SERVER_H
 #define CHAT_SERVER_H
+
+struct event_infor;
+
+typedef std::function<void(event_infor *)> EventCallback;
+
+struct event_infor {
+    std::string ip;
+    u_int port;
+    int fd;
+    EventCallback eventCallback;
+    int events;
+//    void *infor;
+};
 
 
 class Server {
@@ -19,6 +37,8 @@ private:
     int lfd;
     u_int _port;
     struct sockaddr_in serv_addr{};
+    int epoll_fd;
+
 public:
     Server(char *ip, u_int port) : _ip(ip), _port(port) {
         lfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -29,17 +49,29 @@ public:
         printf("Using ip:%s port: %d\n", _ip, _port);
         bind(lfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
         listen(lfd, 128);
+        epoll_fd = epoll_create(128);
     }
 
 
     void run();
     void eventset();
-    void eventadd();
     void eventdel();
 
-    void eventadd(int lfd);
 
-    void eventadd(int lfd, int epoll_fd);
+    void initsocket();
+    void recvdata(event_infor *infor);
+
+    void test();
+
+    void acceptconn(event_infor *infor);
+
+    void initsocket(event_infor &l_infor);
+
+    void eventadd(int events, event_infor &infor);
+
+    void eventadd(int events, event_infor *infor);
+
+    void initsocket(event_infor *l_infor);
 };
 
 
